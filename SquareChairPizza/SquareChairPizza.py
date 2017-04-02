023df3d5f2c7c9abf44feb94cd08d3cd3f13dc6c
Rick <Rick@WEMBLEY>
2017-04-02 05:37:23 -0400

    Add project files.

---
+++ SquareChairPizza/SquareChairPizza.py
@@ -0,0 +1,9 @@
+#Project: CIS 177 WEEK 10 PROJECT
+#Project Location: projects\cis177\SquareChairPizza
+#File: SquareChairPizza.py
+#Purpose: Customer Tracking System for Square Chair Pizza
+#         using a dict to store customer data
+#Revision: 1.0 / 1 APR 2017
+#Created: 1 APR 2017
+#Author: Rick Miller <rick@rickthegeek.com>
+
